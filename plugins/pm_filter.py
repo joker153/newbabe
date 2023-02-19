4fe68dee375ee88fcb52e204dae526738e0e8d74
joker153 <77005994+joker153@users.noreply.github.com>
2023-02-19 11:29:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -723,8 +723,8 @@ async def auto_filter(client, msg, spoll=False):
     else:
         fmsg = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
         
-        await asyncio.sleep(300)
-        await fmsg.delete()
+    await asyncio.sleep(300)
+    await fmsg.delete()
 
     if spoll:
         await msg.message.delete()
