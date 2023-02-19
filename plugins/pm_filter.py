5bc4a820f079f07fe3b6bb59baeee336d8c71457
joker153 <77005994+joker153@users.noreply.github.com>
2023-02-19 11:56:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,24 +64,37 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
-            ]
+                    text=f"📂 [{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}#{query.from_user.id}'
+                )
+            ] 
             for file in files
         ]
     else:
-        btn = [
+        btn = [        
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}", callback_data=f'{pre}#{file.file_id}#{query.from_user.id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'files_#{file.file_id}',
-                ),
-            ]
+                    callback_data=f'{pre}_#{file.file_id}#{query.from_user.id}',
+                )
+            ] 
             for file in files
         ]
+
+    btn.insert(0, 
+        [
+            InlineKeyboardButton(f'♨️ {search} ♨️ ', 'dupe')
+        ]
+    )
+    btn.insert(1,
+        [ 
+            InlineKeyboardButton(f'ᴍᴏᴠɪᴇs', 'dupe'),
+            InlineKeyboardButton(f'sᴇʀɪᴇs', 'dupe'),
+            InlineKeyboardButton(f'ᴛɪᴘs', 'tips')
+        ]
+    )
 
     if 0 < offset <= 10:
         off_set = 0
