eebd22101545246595e5d42abc178d4439f27a6e
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-09 23:23:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,7 +6,7 @@ import time
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
-from script import MVS_TXT, SRS_TXT
+from Script import MVS_TXT, SRS_TXT
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
