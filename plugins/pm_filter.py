f0d9b7664324c978d21c2b24b24f36ac7ace24a2
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-09 20:36:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -922,7 +922,6 @@ async def manual_filters(client, message, text=False):
         return False
 
     async def global_filters(client, message, text=False):
-    settings = await get_settings(message.chat.id)
     group_id = message.chat.id
     name = text or message.text
     reply_id = message.reply_to_message.id if message.reply_to_message else message.id
